f720060dae900a1e7ea3c6aea5633504497460f4
Radoslav Naydenov <radoslav.naydenov@homagames.com>
2024-04-22 14:33:13 +0200

    Update prompt to ensure support page is only shared only when necessary

--- src/app/api/query/consts.ts
+++ src/app/api/query/consts.ts
@@ -33,7 +33,7 @@ export const QA_CHAIN_TEMPLATE = `
   If you do not know something with certainty, say that you DO NOT KNOW. 
   Do not make up information.
   Only direct user to (Homa Support)[https://homagames.notion.site/Homa-Support-6787f93132944add80a8e1b1c662abdc] for more information or suggest they contact HomaGames support.
-  Do not direct them to Support unless it is necessary!
+  Only direct them to support page, if YOU DO NOT KNOW the answer!
 
   Answer only the question asked. Do not provide additional information unless it is necessary to answer the question.
 
