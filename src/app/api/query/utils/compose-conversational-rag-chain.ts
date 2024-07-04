18548653e982053560fa67432cb7e292381987d1
Radoslav Naydenov <radoslav.naydenov@homagames.com>
2024-07-04 17:40:18 +0200

    imrpove bot answers

--- src/app/api/query/utils/compose-conversational-rag-chain.ts
+++ src/app/api/query/utils/compose-conversational-rag-chain.ts
@@ -1,30 +1,57 @@
 import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
 import { RunnableSequence, RunnablePassthrough, RunnableWithMessageHistory, RunnableConfig } from "@langchain/core/runnables";
 import { QA_CHAIN_TEMPLATE } from './consts';
+// import { MultiQueryRetriever } from "langchain/retrievers/multi_query";
 import { ComposeConversationalContextChainArgs } from './types';
 import { convertDocsToWrappedString } from "./convert-docs-to-string";
 import { contextualizedQuestion } from "./contextualized-question";
 import { getMessageHistoryForSessionID } from "./get-message-history-for-session-id";
-import { ChatOpenAI } from "@langchain/openai";
 
 const createContextSummaryChain = (llm: any) => {
   const contextSummaryPrompt = ChatPromptTemplate.fromTemplate(`
-  Based on the following information, provide a brief internal summary of the user's context:
-
-  ONLY adhere to information provided in the context.
+  Based on the provided information, particularly the user's role, summarize the their ability to perform the requested action:
 
   User Info: {user_info}
   Context: {context}
   Current Question: {question}
 
-  Summarize the user's main ask and whether they, given their role, can carry out the task themselves or will need support from <X> member of the team. 
+  ONLY use information provided in the context. Do not invent or assume additional details.
+
+  ## Output Format:
+
+  Action: [Briefly describe the action the user is attempting]
+  Current Status: [Describe the current state of the action, including if the user has completed any prerequisites]
+  What is left: [Describe the remaining steps or prerequisites] 
+  Action Permissions: [Who has the authority to perform the pending action(s)?]
+  Next steps: [Briefly describe what the user should do / who they should contact next]
+
+  Keep the summary concise and directly relevant to the user's current request.
+  Do not mention the user's role explicitly in the output.
 `);
 
-  return RunnableSequence.from([
-    contextSummaryPrompt,
-    llm,
-  ]);
+  return RunnableSequence.from([contextSummaryPrompt, llm]);
 };
+
+// TODO: Cool, but not really needed. FOR NOW.
+// const generateQueries = async (question: string, llm: any, retriever: any) => {
+//   const retrieve = (question: string) => MultiQueryRetriever.fromLLM({
+//     llm,
+//     retriever: retriever,
+//     prompt: ChatPromptTemplate.fromMessages(
+//       ['system', `Given the following question, please generate 2-3 subqueries that would help in answering the main question. Focus on key concepts and actions mentioned.
+
+//       Original question: {question}
+
+//       Subqueries:
+//       1.
+//       2.
+//       3. (optional)
+//       `],
+//     ),
+//   }).invoke(question);
+
+//   return await retrieve(question);
+// };
 
 export const composeConversationalContextChain = async ({
   sessionId,
@@ -46,22 +73,22 @@ export const composeConversationalContextChain = async ({
     RunnablePassthrough.assign({
       context: async (input: Record<string, unknown>) => {
         if ("history" in input) {
-          const chain = contextualizedQuestion(input, { llm }) as RunnableSequence;
-          const docs = await chain.pipe(retriever).invoke(input);
+          const chain = contextualizedQuestion(input, { llm }) as RunnableSequence
+          const docs = await chain.pipe(retriever).invoke(input)
 
           return convertDocsToWrappedString(docs);
         }
         return "";
       },
-      user_info: (input: Record<string, unknown>) => "The user is an external developer",
     }),
     RunnablePassthrough.assign({
       context_summary: async (input: Record<string, string>) => {
         const summary = await contextSummaryChain.invoke({
-          user_info: input.user_info,
+          user_info: "The user is an external developer",
           context: input.context,
           question: input.question
         });
+        console.log("🚀 ~ context_summary: ~ summary.content:", summary.content)
         return summary.content;
       }
     }),
